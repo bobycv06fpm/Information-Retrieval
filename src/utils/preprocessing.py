7c2605ad65d5f6121339f361c9d0f8eed8355a5c
Victor Tolmachev <tolmachev.2406@gmail.com>
2020-04-24 19:47:03 +0300

    Add preprocessing

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,16 +1,50 @@
 import re
 
 import contractions
+import fire
+import pandas as pd
 from nltk import pos_tag
 from nltk.corpus import wordnet
 from nltk.stem import SnowballStemmer, WordNetLemmatizer
 from nltk.tokenize import TweetTokenizer
 from segtok.tokenizer import split_possessive_markers
 
+from src import coord
 from src.utils.stop_words import EN_STOP_WORDS
 
 
 class TextPreprocessor:
+    """Class for tweet preprocessing.
+
+    It can be used as pipeline or you can use certain functions
+    to preprocess tweet.
+
+    Args:
+        lowercase (bool, optional): whether to lowercase text or not.
+            Defaults to True.
+        clean_links (bool, optional): whether to clean links and pictures
+            from tweet. Defaults to True.
+        clean_punctuation (bool, optional): whether to clean punctuation.
+            Defaults to True.
+        expand_contractions (bool, optional): whether to expand contractions.
+            Defaults to False.
+        remove_stop_words (bool, optional): whetehr to remove stop words.
+            Defaults to True.
+        process_numbers (str, optional): method to process numbers.
+            Possible values: 'remove' (removes all numbers),
+            'replace' (replace all digits by 0).  Defaults to 'remove'.
+        normalization_type (str, optional): type of normalization.
+            Possible values: 'lemma', 'stem'. Defaults to 'lemma'.
+        clean_mentions (bool, optional): clean all mentions. Defaults to False.
+        tokenizer (callable, optional): totokenizer (it should receive
+            a function that processes text and returns list of tokens).
+            If None, then nltk.TweetTokenizer will be used. Defaults None.
+        stop_words (list, optional): list of stop words. If None EN_STOP_WORDS
+            from stop_words.py will be used. Defaults to None.
+
+    Returns:
+        list: list of tokens.
+    """
 
     def __init__(self,
                  lowercase=True,
@@ -23,6 +57,7 @@ class TextPreprocessor:
                  clean_mentions=False,
                  tokenizer=None,
                  stop_words=None):
+
         self._lowercase = lowercase
         self._clean_links = clean_links
         self._expand_contractions = expand_contractions
@@ -137,6 +172,14 @@ class TextPreprocessor:
         return tokens
 
     def get_wordnet_pos(self, token):
+        """Returns part of speech for a given token.
+
+        Args:
+            token (str): token to process.
+
+        Returns:
+            wordnet part of speech.
+        """
         tag_dict = {
             "J": wordnet.ADJ,
             "N": wordnet.NOUN,
@@ -153,6 +196,66 @@ class TextPreprocessor:
         return text
 
     def __call__(self, text):
+        """Applies whole pipeline to the text.
+
+        Args:
+            text (str): text to preprocess.
+
+        Returns:
+            list of tokens: list of preprocessed tokens.
+        """
         for processor in self.pipeline:
             text = processor(text)
         return text
+
+
+def basic_preprocess(src=coord.data_raw,
+                     dest=coord.data_interim.joinpath('dataset_v1.jsonl'),
+                     min_tweet_len=50):
+    """Preprocesses folder with raw data.
+
+    This method cleans all links, drops tweets with less than min_tweet_len
+        parameter and gathers all users into one dataset.
+
+    Args:
+        src (str, optional): folder with raw data. This folder contains jsonl
+            files for each scraped user. Defaults to data/raw/.
+        dest (str, optional): output folder with preprocessed data.
+            Defaults to data/interim/dataset_v1.jsonl.
+        min_tweet_len (int, optional): minimum tweet length to remain.
+            Defaults to 50.
+    """
+
+    dataset = pd.DataFrame()
+    for file_path in src.iterdir():
+        if file_path.name.endswith('jsonl'):
+            user_df = pd.read_json(file_path, lines=True)
+            dataset = dataset.append(user_df, ignore_index=True)
+
+    dataset['quoted_tweet_screen_name'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['screen_name'])
+    dataset['quoted_tweet_name'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['name'])
+    dataset['quoted_tweet_hashtags'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['hashtags'])
+    dataset['quoted_tweet_mentions'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['mentions'])
+    dataset['quoted_tweet_n_emojis'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['n_emojis'])
+    dataset['quoted_tweet'] = dataset['quoted_tweet'].apply(
+        lambda tweet: tweet['text'])
+
+    preprocessor = TextPreprocessor()
+    dataset['tweet'] = dataset['tweet'].apply(
+        lambda tweet: preprocessor.clean_links(tweet))
+    dataset['quoted_tweet'] = dataset['quoted_tweet'].apply(
+        lambda tweet: preprocessor.clean_links(tweet))
+
+    dataset = dataset[dataset['tweet'].apply(lambda tweet: sum(
+        len(token) for token in tweet.split()) >= min_tweet_len)]
+
+    dataset.to_json(dest, lines=True, orient='records')
+
+
+if __name__ == '__main__':
+    fire.Fire({'basic_preprocess': basic_preprocess})
